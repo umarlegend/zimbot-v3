0655739e5d533f6bcb38d0846419c618c0e25243
DARK UMAR <103824676+umarlegend@users.noreply.github.com>
2022-10-08 23:45:01 +0500

    Update config.js
    Signed-off-by: DARK UMAR <103824676+umarlegend@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.owner = ['966594204996','']
+global.premium = ['923165123719','966594204996']
+global.botname = 'SANWAL 570'
+global.ownername = 'UMAR'
+global.group1 = 'https://chat.whatsapp.com/BBQgiM0adHc1m3wM6fEw5Y'
+global.group2 = 'https://chat.whatsapp.com/BBQgiM0adHc1m3wM6fEw5Y'
+global.packname = 'UNAR BOT INC'
+global.ytchannel = 'https://www.youtube.com/c/UMARMODS'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -65,7 +65,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = 'SANWAL 570'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
